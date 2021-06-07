1b392421b6ee6eee6a4172fef531dd639b84c5d3
Tan Phat <nguyentanphat30@gmail.com>
2021-06-07 22:35:00 +0700

    update app.js

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-//Cloudinary upload API constants
+//Khai báo tên tài khoản cloudinary
 const CLOUD_NAME = "dz5pvwzm5";
+
+//Khai báo địa chỉ chứa data thu âm thanh
 const CLOUDINARY_URL = `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/auto/upload`;
 
+//Các endpoint ứng với từng lệnh 
 const COMMAND_1_PRESET  = "cjn01rjd";
 const COMMAND_2_PRESET  = "gglajrqs";
 const COMMAND_3_PRESET  = "nzpqpddv";
@@ -12,21 +15,18 @@ const COMMAND_7_PRESET  = "oy4tyjh9";
 const COMMAND_8_PRESET  = "e5hq3h9t";
 const COMMAND_9_PRESET  = "kmyl6gqf";
 const COMMAND_10_PRESET = "jppru3wt";
-const COMMAND_11_PRESET = "xn0ajz9k";
-const COMMAND_12_PRESET = "hhrz2wxs";
 
-//webkitURL is deprecated but nevertheless 
 URL = window.URL || window.webkitURL;
 var gumStream;
 //stream from getUserMedia() 
 var rec;
-//Recorder.js object 
+//đối tượng Recorder.js  
 var input;
-//MediaStreamAudioSourceNode we'll be recording 
-// shim for AudioContext when it's not avb. 
 var AudioContext = window.AudioContext || window.webkitAudioContext;
 var audioContext = new AudioContext;
-//new audio context to help us record 
+//audio context giúp thu âm
+
+//Khai báo các nút, thanh trạng thái
 var recordButton = document.getElementById("recordButton");
 var uploadButton = document.getElementById("uploadButton");
 var recordCount = document.getElementById("recordCount");
@@ -35,21 +35,22 @@ var nextButton = document.getElementById("next");
 var statusBar = document.getElementById("status");
 var readyBar = document.getElementsByClassName("ready")[0];
 
-//add events to those 3 buttons 
+//Gắn sự kiện cho nút thu âm, upload file, đặt chế độ hiển thị cho nút upload và nút tiếp theo
 recordButton.addEventListener("click", startRecording);
 uploadButton.addEventListener("click", uploadFiles);
 uploadButton.disabled = true;
 nextButton.hidden = true;
+
+//Khởi tạo các giá trị đầu
+const ATTEMPS_PER_COMMAND = 10;
 var audioTracks = [];
 var trackCount;
 var uploadCount = 0;
-/* Simple constraints object, for more advanced audio features see
 
-https://addpipe.com/blog/audio-constraints-getusermedia/ */
-
+//Hàm kiểm tra xem 1 lệnh đủ 10 file hay chưa
 function check(){
      recordCount.innerHTML = "RECORDED: " + audioTracks.length;
-     if (audioTracks.length == 10){
+     if (audioTracks.length == ATTEMPS_PER_COMMAND){
           recordButton.disabled = true;
           uploadButton.disabled = false;
      } else {
@@ -58,79 +59,46 @@ function check(){
      }
 } 
 
+//Hàm thu âm
 function startRecording() {
 	console.log("recordButton clicked");
      readyBar.innerHTML = "Xin mời nói!"
-	/*
-		Simple constraints object, for more advanced audio features see
-		https://addpipe.com/blog/audio-constraints-getusermedia/
-	*/
-    
-    var constraints = { audio: true, video:false }
-
- 	/*
-    	Disable the record button until we get a success or fail from getUserMedia() 
-	*/
-
+     var constraints = { audio: true, video:false }
 	recordButton.disabled = true;
 
-	/*
-    	We're using the standard promise based getUserMedia() 
-    	https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
-	*/
-
 	navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
-		console.log("getUserMedia() success, stream created, initializing Recorder.js ...");
+		console.log("getUserMedia() thành công, tạo luồng, khởi chạy Recorder.js ...");
 
-		/*
-			create an audio context after getUserMedia is called
-			sampleRate might change after getUserMedia is called, like it does on macOS when recording through AirPods
-			the sampleRate defaults to the one set in your OS for your playback device
-		*/
 		audioContext = new AudioContext();
 		audioContext.resume().then(() => {
-               //update the format 
-		//document.getElementById("formats").innerHTML="Format: 1 channel pcm @ "+audioContext.sampleRate/1000+"kHz"
 
-		/*  assign to gumStream for later use  */
 		gumStream = stream;
-		
-		/* use the stream */
 		input = audioContext.createMediaStreamSource(stream);
-
-		/* 
-			Create the Recorder object and configure to record mono sound (1 channel)
-			Recording 2 channels  will double the file size
-		*/
 		rec = new Recorder(input,{numChannels:1})
-
-		//start the recording process
 		rec.record()
 
-		console.log("Recording started");
+		console.log("Bắt đầu thu âm");
           });
-          
-          setTimeout(stopRecording, 1600);
-		
 
+          setTimeout(stopRecording, 1600);
 	}).catch(function(err) {
-	  	//enable the record button if getUserMedia() fails
-    	recordButton.disabled = false;
+          //Cho nút thu âm hoạt động trở lại khi getUserMedia() bị lỗi
+    	     recordButton.disabled = false;
 	});
 }
 
+//Hàm dừng thu âm
 function stopRecording() {
      console.log("stopButton clicked");
      readyBar.innerHTML = "";
-     //disable the stop button, enable the record too allow for new recordings 
      recordButton.disabled = false;
-     //tell the recorder to stop the recording 
-     rec.stop(); //stop microphone access 
+     rec.stop(); //dừng truy cập microphone 
      gumStream.getAudioTracks()[0].stop();
-     //create the wav blob and pass it on to createDownloadLink 
+     //Tạo file wav bằng kiểu blob và truyền vào hàm createDownloadLink
      rec.exportWAV(createDownloadLink);
 }
 
+//Hàm xóa file audio
 function deleteAudioTrack(delBtn, li, blob){
      li.remove();
      delBtn.remove(); 
@@ -141,6 +109,7 @@ function deleteAudioTrack(delBtn, li, blob){
      check();
 }
 
+//Hàm tạo nút download
 function createDownloadLink(blob) {
      var url = URL.createObjectURL(blob);
      var au = document.createElement('audio');
@@ -148,26 +117,23 @@ function createDownloadLink(blob) {
      var link = document.createElement('a');
      var delBtn = document.createElement('button');
      delBtn.innerHTML = "X";
-     //add file to audioTracks
+     //Thêm file vào mảng audioTracks
      audioTracks.push(blob);
-     //add controls to the <audio> element 
      au.controls = true;
      au.src = url;
-     //link the a element to the blob 
      link.href = url;
      link.download = new Date().toISOString() + '.wav';
      link.innerHTML = link.download;
-     //add the new audio and a elements to the li element 
      li.appendChild(au);
      li.appendChild(link);
      recordingsList.appendChild(delBtn); 
-     //add the li element to the ordered list 
      recordingsList.appendChild(li);
      delBtn.addEventListener("click", () => deleteAudioTrack(delBtn, li, blob));
      console.log(audioTracks);
      check();
 }
 
+//Hàm upload lên cloudinary
 function uploadFiles(){
      uploadButton.disabled = true;
      var pathNumber = parseInt(recordButton.className);
@@ -184,7 +150,7 @@ function uploadFiles(){
           }).then((res) => {
                uploadCount++;
                statusBar.innerHTML = "Uploaded " + uploadCount + " file(s)";
-               if(uploadCount === 10){
+               if(uploadCount === ATTEMPS_PER_COMMAND){
                     statusBar.innerHTML = "Upload thành công! Click Next để tiếp tục...";
                     nextButton.hidden = false;
                }
@@ -193,5 +159,4 @@ function uploadFiles(){
                console.error(err);
           })
      }
-
 }
